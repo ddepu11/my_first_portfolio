ef356bf8f3894d249eb849edc42d987d478cfe67
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-18 21:42:04 +0530

    chore: add projects array

---
+++ src/projects.ts
@@ -0,0 +1,43 @@
+const projects = [
+  {
+    title: 'Movil Shop',
+    description: '',
+    liveLink: 'https://movil-shop.netlify.app/',
+    codeLink: 'https://github.com/ddepu11/movil_shop_client_side_react',
+    images: [],
+  },
+
+  {
+    title: 'Sangeetmay',
+    description: '',
+    liveLink: 'https://sangeetmay.netlify.app/',
+    codeLink: 'https://github.com/ddepu11/sangeetmay',
+    images: [],
+  },
+
+  {
+    title: 'Chitransh',
+    description: '',
+    liveLink: 'https://chitransh.netlify.app/',
+    codeLink: 'https://github.com/ddepu11/Chitransh',
+    images: [],
+  },
+
+  {
+    title: 'Movies DB',
+    description: '',
+    liveLink: 'https://movies-dbt.netlify.app/',
+    codeLink: 'https://github.com/ddepu11/movies-db',
+    images: [],
+  },
+
+  {
+    title: 'Social Chat Rooms',
+    description: '',
+    liveLink: 'https://social-chat-rooms.netlify.app/',
+    codeLink: 'https://github.com/ddepu11/social_chat_rooms',
+    images: [],
+  },
+];
+
+export default projects;
