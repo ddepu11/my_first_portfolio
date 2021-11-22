08f041ad9d5b89303c3c397a7f59f5ef527407af
Deepanshu Tiwari <ddepu11@gmail.com>
2021-11-22 22:03:46 +0530

    refactor: change index of projects

--- src/projects.ts
+++ src/projects.ts
@@ -2,43 +2,6 @@ import { IProject } from './interfaces';
 
 const projects: IProject[] = [
   {
-    title: 'Movil Shop',
-    description:
-      "This is an E-commerce web app for selling mobiles made with MERN stack and it has features like add to cart, product details, Login, Signup, Update account info, add your billing address, Seller and Admin dashboard, Sellers can add mobile with it's images he can also update it's details, Admin can delete users and sellers, also used Styled-Components for styling, Redux for state management, Authentication using JWT, Login via Google. Also used Eslint for linting with airbnb style guide and Prettier for formating",
-    liveLink: 'https://movil-shop.netlify.app/',
-    codeLink: 'https://github.com/ddepu11/movil_shop_client_side_react',
-    images: [
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/1.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/2.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/3.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/4.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/5.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/6.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/7.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/8.png?raw=true',
-    ],
-  },
-
-  {
-    title: 'Sangeetmay',
-    description:
-      "It's a Music Player web app it has features like login, Signup, logout, Update account info, list songs and playlists, list liked songs, play/pause song, mute/unmute volume, like or dislike the song, Admin can upload and delete song with its image. Made it with TypeScript, React, Styled-components, Webpack 5, React-icons, Firebase. Also used Eslint for linting and Prettier for formating",
-    liveLink: 'https://sangeetmay.netlify.app/',
-    codeLink: 'https://github.com/ddepu11/sangeetmay',
-    images: [
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/1.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/2.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/3.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/4.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/5.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/6.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/7.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/8.png?raw=true',
-      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/9.png?raw=true',
-    ],
-  },
-
-  {
     title: 'Chitransh',
     description:
       "It's a social media web app where users can login, signup, Create post with caption and upload multiple images, like/dislike/save posts and  make a comment on it, search people, follow and unfollow people, see people profile with their following and followers list, update your profile pictures and account information, see people to follow suggestion list. i have used tech like React, Styled-components, Webpack 5, Material-icons, Firebase. Also used Eslint for linting and Prettier for formating ",
@@ -63,6 +26,24 @@ const projects: IProject[] = [
   },
 
   {
+    title: 'Movil Shop',
+    description:
+      "This is an E-commerce web app for selling mobiles made with MERN stack and it has features like add to cart, product details, Login, Signup, Update account info, add your billing address, Seller and Admin dashboard, Sellers can add mobile with it's images he can also update it's details, Admin can delete users and sellers, also used Styled-Components for styling, Redux for state management, Authentication using JWT, Login via Google. Also used Eslint for linting with airbnb style guide and Prettier for formating",
+    liveLink: 'https://movil-shop.netlify.app/',
+    codeLink: 'https://github.com/ddepu11/movil_shop_client_side_react',
+    images: [
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/1.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/2.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/3.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/4.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/5.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/6.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/7.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movil-shop/8.png?raw=true',
+    ],
+  },
+
+  {
     title: 'Movies DB',
     description:
       "As it's name suggests it's a app which consumes themoviedb api and lists movies, it has features like login, signup, like/dislike and save to watch later movie, wath movies  trailers/clips, create a new playlist and add movie in it, list movies accoring to genere and scroll down to fetch more movies, view movie details. Made this app using, React, Styled-components, Webpack 5, Material-icons, Firebase. And also used Eslint for linting and  Prettier for formating",
@@ -77,6 +58,25 @@ const projects: IProject[] = [
       'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movies-db/6.png?raw=true',
       'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movies-db/7.png?raw=true',
       'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/movies-db/8.png?raw=true',
+    ],
+  },
+
+  {
+    title: 'Sangeetmay',
+    description:
+      "It's a Music Player web app it has features like login, Signup, logout, Update account info, list songs and playlists, list liked songs, play/pause song, mute/unmute volume, like or dislike the song, Admin can upload and delete song with its image. Made it with TypeScript, React, Styled-components, Webpack 5, React-icons, Firebase. Also used Eslint for linting and Prettier for formating",
+    liveLink: 'https://sangeetmay.netlify.app/',
+    codeLink: 'https://github.com/ddepu11/sangeetmay',
+    images: [
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/1.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/2.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/3.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/4.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/5.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/6.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/7.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/8.png?raw=true',
+      'https://github.com/ddepu11/my_first_portfolio/blob/main/projects/sangeetmay/9.png?raw=true',
     ],
   },
 
