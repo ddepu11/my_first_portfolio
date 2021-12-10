7ad50084e6d264164872fb37191e66f384a757fd
Deepanshu Tiwari <ddepu11@gmail.com>
2021-12-10 13:29:46 +0530

    chore: trim project details

--- src/projects.ts
+++ src/projects.ts
@@ -4,7 +4,7 @@ const projects: IProject[] = [
   {
     title: 'Chitransh',
     description:
-      "It's a social media web app where users can login, signup, Create post with caption and upload multiple images, like/dislike/save posts and  make a comment on it, search people, follow and unfollow people, see people profile with their following and followers list, update your profile pictures and account information, see people to follow suggestion list. i have used tech like React, Styled-components, Webpack 5, Material-icons, Firebase. Also used Eslint for linting and Prettier for formating ",
+      "It's a social media web app where users can create post with caption and upload multiple images, like/dislike/save posts and  make a comment on it, search people, follow and unfollow people, see people profile with their following and followers list, see people to follow suggestion list.I have used tech like React, styled-components, Webpack 5, Material-icons, Firebase.",
     liveLink: 'https://chitransh.netlify.app/',
     codeLink: 'https://github.com/ddepu11/Chitransh',
     images: [
@@ -28,7 +28,7 @@ const projects: IProject[] = [
   {
     title: 'Movil Shop',
     description:
-      "This is an E-commerce web app for selling mobiles made with MERN stack and it has features like add to cart, product details, Login, Signup, Update account info, add your billing address, Seller and Admin dashboard, Sellers can add mobile with it's images he can also update it's details, Admin can delete users and sellers, also used Styled-Components for styling, Redux for state management, Authentication using JWT, Login via Google. Also used Eslint for linting with airbnb style guide and Prettier for formating",
+      "This is an E-commerce web app for selling mobiles made with MERN stack and it has features like add to cart, place an order, add your billing address, Seller and Admin dashboard, Sellers can add mobile with it's images he can also update it's details, Admin can delete users and sellers, also used Styled-Components for styling, Redux for state management, Authentication using JWT, Login via Google.",
     liveLink: 'https://movil-shop.netlify.app/',
     codeLink: 'https://github.com/ddepu11/movil_shop_client_side_react',
     images: [
@@ -46,7 +46,7 @@ const projects: IProject[] = [
   {
     title: 'Movies DB',
     description:
-      "As it's name suggests it's a app which consumes themoviedb api and lists movies, it has features like login, signup, like/dislike and save to watch later movie, wath movies  trailers/clips, create a new playlist and add movie in it, list movies accoring to genere and scroll down to fetch more movies, view movie details. Made this app using, React, Styled-components, Webpack 5, Material-icons, Firebase. And also used Eslint for linting and  Prettier for formating",
+      "As it's name suggests it's a app which consumes themoviedb api and lists movies, it has features like/dislike and save to watch later movie, wath movie's trailers/clips, create a new playlist and add movie in it, list movies accoring to genere and scroll down to fetch more movies, view movie details. Made this app using, React, Styled-components, Webpack 5, Material-icons, Firebase.",
     liveLink: 'https://movies-dbt.netlify.app/',
     codeLink: 'https://github.com/ddepu11/movies-db',
     images: [
@@ -64,7 +64,7 @@ const projects: IProject[] = [
   {
     title: 'Sangeetmay',
     description:
-      "It's a Music Player web app it has features like login, Signup, logout, Update account info, list songs and playlists, list liked songs, play/pause song, mute/unmute volume, like or dislike the song, Admin can upload and delete song with its image. Made it with TypeScript, React, Styled-components, Webpack 5, React-icons, Firebase. Also used Eslint for linting and Prettier for formating",
+      "It's a Music Player web app it has features like play/pause song, mute/unmute volume, like or dislike the song,  list songs and playlists, list liked songs, Admin can upload and delete song with its image. Made it with TypeScript, React, Styled-components, Webpack 5, React-icons, Firebase.",
     liveLink: 'https://sangeetmay.netlify.app/',
     codeLink: 'https://github.com/ddepu11/sangeetmay',
     images: [
@@ -83,7 +83,7 @@ const projects: IProject[] = [
   {
     title: 'Social Chat Rooms',
     description:
-      "It's a simple messaging web app where people can create chat rooms and have chat with random people, users can login, signup, logout, update his account info. Made it with React, Styled-components, Webpack 5, React-icons, Firebase. Also used Eslint for linting and  Prettier for formating.",
+      "It's a simple messaging web app where people can create chat rooms and have chat with random people, users can  update his account info. Made it with React, Styled-components, Webpack 5, React-icons, Firebase.",
     liveLink: 'https://social-chat-rooms.netlify.app/',
     codeLink: 'https://github.com/ddepu11/social_chat_rooms',
     images: [
